85d81ffe8ef878247af528a33af6d1262d2e704f
Mohammed Sazeed <mohammed.sazeed@bizcloudexperts.com>
2023-08-10 20:59:45 +0530

    pushing M1 code

---
+++ src/M1/netsuite_ar_m1.js
@@ -0,0 +1,465 @@
+const AWS = require("aws-sdk");
+const crypto = require("crypto");
+const OAuth = require("oauth-1.0a");
+const axios = require("axios");
+const {
+  getConfig,
+  getConnectionToRds,
+  createARFailedRecords,
+  triggerReportLambda,
+  sendDevNotification,
+} = require("../../Helpers/helper");
+const { getBusinessSegment } = require("../../Helpers/businessSegmentHelper");
+
+let userConfig = "";
+let connections = "";
+
+const arDbNamePrev = process.env.DATABASE_NAME;
+const arDbName = arDbNamePrev + "interface_ar";
+const source_system = "M1";
+let totalCountPerLoop = 20;
+const today = getCustomDate();
+
+module.exports.handler = async (event, context, callback) => {
+  userConfig = getConfig(source_system, process.env);
+  let hasMoreData = "false";
+  let currentCount = 0;
+  totalCountPerLoop = event.hasOwnProperty("totalCountPerLoop")
+    ? event.totalCountPerLoop
+    : totalCountPerLoop;
+
+  try {
+    /**
+     * Get connections
+     */
+    connections = await getConnectionToRds(process.env);
+
+    /**
+     * Get data from db
+     */
+    const orderData = await getDataGroupBy(connections);
+
+    console.log("orderData", orderData.length, orderData[0]);
+    const invoiceIDs = orderData.map((a) => "'" + a.invoice_nbr + "'");
+    console.log("invoiceIDs", invoiceIDs);
+
+    currentCount = orderData.length;
+    const invoiceDataList = await getInvoiceNbrData(connections, invoiceIDs);
+    console.log("invoiceDataList", invoiceDataList.length);
+
+    /**
+     * 5 simultaneous process
+     */
+    const perLoop = 15;
+    let queryData = [];
+    for (let index = 0; index < (orderData.length + 1) / perLoop; index++) {
+      let newArray = orderData.slice(
+        index * perLoop,
+        index * perLoop + perLoop
+      );
+
+      const data = await Promise.all(
+        newArray.map(async (item) => {
+          return await mainProcess(item, invoiceDataList);
+        })
+      );
+      queryData = [...queryData, ...data];
+    }
+
+    console.log("queryData", queryData);
+    await updateInvoiceId(connections, queryData);
+
+    if (currentCount > totalCountPerLoop) {
+      hasMoreData = "true";
+    } else {
+      await triggerReportLambda(process.env.NS_RESTLET_INVOICE_REPORT, "M1_AR");
+      await startNextStep();
+      hasMoreData = "false";
+    }
+    return { hasMoreData };
+  } catch (error) {
+    await triggerReportLambda(process.env.NS_RESTLET_INVOICE_REPORT, "M1_AR");
+    await startNextStep();
+    return { hasMoreData: "false" };
+  }
+};
+
+/**
+ * main process of netsuite AR API
+ * @param {*} connections
+ * @param {*} item
+ */
+async function mainProcess(item, invoiceDataList) {
+  let singleItem = null;
+  try {
+    /**
+     * get invoice obj from DB
+     */
+    const dataList = invoiceDataList.filter((e) => {
+      return (
+        e.invoice_nbr == item.invoice_nbr && e.invoice_type == item.invoice_type
+      );
+    });
+
+    singleItem = dataList[0];
+    console.log("singleItem", singleItem);
+    /**
+     * Make Json payload
+     */
+    const jsonPayload = await makeJsonPayload(dataList);
+
+
+    /**
+     * create Netsuit Invoice
+     */
+    const invoiceId = await createInvoice(jsonPayload, singleItem);
+    console.log("invoiceId", invoiceId);
+
+    /**
+     * update invoice id
+     */
+    const getQuery = getUpdateQuery(singleItem, invoiceId);
+    return getQuery;
+  } catch (error) {
+    console.log("error:process", error);
+    if (error.hasOwnProperty("customError")) {
+      let getQuery = "";
+      try {
+        getQuery = getUpdateQuery(singleItem, null, false);
+        await createARFailedRecords(
+          connections,
+          singleItem,
+          error,
+          arDbNamePrev
+        );
+        return getQuery;
+      } catch (error) {
+        await createARFailedRecords(
+          connections,
+          singleItem,
+          error,
+          arDbNamePrev
+        );
+        return getQuery;
+      }
+    }
+  }
+}
+
+async function getDataGroupBy(connections) {
+  try {
+    const query = `SELECT distinct invoice_nbr, invoice_type FROM ${arDbName} where
+    ((internal_id is null and processed is null and customer_internal_id is not null) or
+    (customer_internal_id is not null and processed ='F' and processed_date < '${today}'))
+    and source_system = '${source_system}' and invoice_nbr is not null
+    limit ${totalCountPerLoop + 1}`;
+
+
+    console.info("query", query);
+    const [rows] = await connections.execute(query);
+    const result = rows;
+    if (!result || result.length == 0) {
+      throw "No data found.";
+    }
+    return result;
+  } catch (error) {
+    console.log("error", error);
+    throw "No data found.";
+  }
+}
+
+async function getInvoiceNbrData(connections, invoice_nbr) {
+  try {
+    const query = `select * from ${arDbName} where source_system = '${source_system}' 
+    and invoice_nbr in (${invoice_nbr.join(",")})`;
+    console.log("query", query);
+
+    const executeQuery = await connections.execute(query);
+    const result = executeQuery[0];
+    if (!result || result.length == 0) {
+      throw "No data found.";
+    }
+    return result;
+  } catch (error) {
+    console.log("error");
+    throw "No data found.";
+  }
+}
+
+async function makeJsonPayload(data) {
+  try {
+    const singleItem = data[0];
+    const hardcode = getHardcodeData();
+
+    /**
+     * head level details
+     */
+    const payload = {
+      custbody_mfc_omni_unique_key:
+        singleItem.invoice_nbr +
+        "-" + 
+        singleItem.customer_id +
+        "-" +
+        singleItem.invoice_type+
+        "-"+
+        singleItem.gc_code, //invoice_nbr,customer_id, invoice_type,gc_code
+      tranid: singleItem.invoice_nbr ?? "",
+      trandate: singleItem.invoice_date
+        ? dateFormat(singleItem.invoice_date)
+        : null,
+      department: hardcode.department.head,
+      class: hardcode.class.head,
+      location: hardcode.location.head,
+      custbody_source_system: hardcode.source_system,//2327
+      entity: singleItem.customer_internal_id ?? "",
+      subsidiary: singleItem.subsidiary ?? "",
+      currency: singleItem.currency_internal_id ?? "",
+      otherrefnum: singleItem.file_nbr ?? "",
+      custbody_mode: singleItem?.mode_name ?? "",//2673
+      custbody_service_level: singleItem?.service_level ?? "",//2674
+      custbody18: singleItem.finalized_date ?? "",//1745
+      custbody9: singleItem.housebill_nbr ?? "",//1730 //here in soap we are passing file_nbr
+      custbody17: singleItem.email ?? "",//1744
+      custbody25: singleItem.zip_code ?? "",//2698
+      custbody19: singleItem.unique_ref_nbr ?? "",//1734
+      item: data.map((e) => {
+        return {
+          // custcol_mfc_line_unique_key:"",
+          item: e.charge_cd_internal_id ?? "",
+          taxcode: e?.tax_code_internal_id ?? "",
+          description: e?.charge_cd_desc ?? "",
+          amount: +parseFloat(e.total).toFixed(2) ?? "",
+          rate: +parseFloat(e.rate).toFixed(2) ?? "",
+          department: hardcode.department.line ?? "",
+          class:
+            hardcode.class.line[
+            e.business_segment.split(":")[1].trim().toLowerCase()
+            ],
+          location: {
+            refName: e.handling_stn ?? "",
+          },
+          custcol_hawb: e.housebill_nbr ?? "",//760
+          custcol3: e.sales_person ?? "",//1167
+          custcol5: e.master_bill_nbr ?? "",//1727
+          custcol2: {
+            refName: e.controlling_stn ?? "",//1166
+          },
+          custcol1: e.ready_date ? e.ready_date.toISOString() : "",//1164
+        };
+      }),
+    };
+
+    console.log("payload", JSON.stringify(payload));
+    return payload;
+  } catch (error) {
+    console.log("error payload", error);
+    await sendDevNotification(
+      source_system,
+      "AR",
+      "netsuite_ar_m1 payload error",
+      data[0],
+      error
+    );
+    throw {
+      customError: true,
+      msg: "Unable to make payload",
+      data: data[0],
+    };
+  }
+}
+
+function getAuthorizationHeader(options) {
+  const oauth = OAuth({
+    consumer: {
+      key: options.consumer_key,
+      secret: options.consumer_secret_key,
+    },
+    realm: options.realm,
+    signature_method: "HMAC-SHA256",
+    hash_function(base_string, key) {
+      return crypto
+        .createHmac("sha256", key)
+        .update(base_string)
+        .digest("base64");
+    },
+  });
+  return oauth.toHeader(
+    oauth.authorize(
+      {
+        url: options.url,
+        method: options.method,
+      },
+      {
+        key: options.token,
+        secret: options.token_secret,
+      }
+    )
+  );
+}
+
+async function createInvoice(payload, singleItem) {
+  try {
+    const endpoiont =
+        singleItem.invoice_type == "IN"
+          ? process.env.NETSUIT_RESTLET_INV_URL
+          : process.env.NETSUIT_RESTLET_CM_URL;
+
+    const options = {
+      consumer_key: userConfig.token.consumer_key,
+      consumer_secret_key: userConfig.token.consumer_secret,
+      token: userConfig.token.token_key,
+      token_secret: userConfig.token.token_secret,
+      realm: userConfig.account,
+      url: endpoiont,
+      method: 'POST',
+    };
+
+    const authHeader = await getAuthorizationHeader(options);
+
+    const configApi = {
+      method: options.method,
+      maxBodyLength: Infinity,
+      url: options.url,
+      headers: {
+        'Content-Type': 'application/json',
+        ...authHeader,
+      },
+      data: JSON.stringify(payload),
+    };
+
+    console.log('configApi', configApi);
+
+    const response = await axios.request(configApi);
+
+    if (response.status === 200 && response.data.status === 'Success') {
+      return response.data.id;
+    } else {
+      throw {
+        customError: true,
+        msg: response.data.reason.replace(/'/g, '`'),
+        payload: JSON.stringify(payload),
+        response: JSON.stringify(response.data).replace(/'/g, '`'),
+      };
+    }
+  } catch (error) {
+    console.log("createInvoice:error", error);
+    if (error.response) {
+      throw {
+        customError: true,
+        msg: error.msg.replace(/'/g, '`'),
+        payload: error.payload,
+        response: error.response.replace(/'/g, '`'),
+      };
+    } else {
+      throw {
+        customError: true,
+        msg: 'Netsuit AR Api Failed',
+        response: '',
+      };
+    }
+  }
+}
+
+
+
+
+function getUpdateQuery(item, invoiceId, isSuccess = true) {
+  try {
+    console.log("invoice_nbr ", item.invoice_nbr, invoiceId);
+    let query = `UPDATE ${arDbName} `;
+    if (isSuccess) {
+      query += ` SET internal_id = '${invoiceId}', processed = 'P', `;
+    } else {
+      query += ` SET internal_id = null, processed = 'F', `;
+    }
+    query += `processed_date = '${today}' 
+              WHERE source_system = '${source_system}' and invoice_nbr = '${item.invoice_nbr}' 
+              and invoice_type = '${item.invoice_type}';`;
+    console.log("query", query);
+    return query;
+  } catch (error) {
+    console.log("error:getUpdateQuery", error, item, invoiceId);
+    return "";
+  }
+}
+
+async function updateInvoiceId(connections, query) {
+  for (let index = 0; index < query.length; index++) {
+    const element = query[index];
+    try {
+      await connections.execute(element);
+    } catch (error) {
+      console.log("error:updateInvoiceId", error);
+      await sendDevNotification(
+        source_system,
+        "AR",
+        "netsuite_ar_m1 updateInvoiceId",
+        "Invoice is created But failed to update internal_id " + element,
+        error
+      );
+    }
+  }
+}
+
+function getHardcodeData() {
+  const data = {
+    source_system: "2",
+    class: {
+      head: "9",
+      line: getBusinessSegment(process.env.STAGE),
+    },
+    department: { head: "15", line: "1" },
+    location: { head: "18", line: "EXT ID: Take from DB" },
+  };
+  return data;
+}
+
+function dateFormat(param) {
+  try {
+    const date = new Date(param);
+    return (
+      date.getFullYear() +
+      "-" +
+      ("00" + (date.getMonth() + 1)).slice(-2) +
+      "-" +
+      ("00" + date.getDate()).slice(-2) +
+      "T11:05:03.000Z"
+    );
+  } catch (error) {
+    return null;
+  }
+}
+
+function getCustomDate() {
+  const date = new Date();
+  let ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
+  let mo = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(date);
+  let da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
+  return `${ye}-${mo}-${da}`;
+}
+
+async function startNextStep() {
+  try {
+    const params = {
+      stateMachineArn: process.env.NETSUITE_VENDOR_STEP_ARN,
+      input: JSON.stringify({}),
+    };
+    const stepfunctions = new AWS.StepFunctions();
+    const data = await new Promise((resolve, reject) => {
+      stepfunctions.startExecution(params, (err, data) => {
+        if (err) {
+          console.info("Netsuit NETSUITE_VENDOR_STEP_ARN trigger failed");
+          reject(err);
+        } else {
+          console.info("Netsuit NETSUITE_VENDOR_STEP_ARN started");
+          resolve(data);
+        }
+      });
+    });
+
+    return true;
+  } catch (error) {
+    console.error("Error in startNextStep:", error);
+    return false;
+  }
+}
---
+++ src/M1/netsuite_customer_ar_m1.js
@@ -0,0 +1,352 @@
+const AWS = require("aws-sdk");
+const axios = require("axios");
+const {
+  getConfig,
+  getConnectionToRds,
+  getAuthorizationHeader,
+  createARFailedRecords,
+  sendDevNotification,
+} = require("../../Helpers/helper");
+const moment = require("moment");
+
+let userConfig = "";
+
+let totalCountPerLoop = 5;
+const today = getCustomDate();
+
+const arDbNamePrev = process.env.DATABASE_NAME;
+const arDbName = arDbNamePrev + "interface_ar";
+const source_system = "M1";
+
+module.exports.handler = async (event, context, callback) => {
+  userConfig = getConfig(source_system, process.env);
+  // const checkIsRunning = await checkOldProcessIsRunning();
+  // if (checkIsRunning) {
+  //   return {
+  //     hasMoreData: "false",
+  //   };
+  // }
+  let hasMoreData = "false";
+  let currentCount = 0;
+  totalCountPerLoop = event.hasOwnProperty("totalCountPerLoop")
+    ? event.totalCountPerLoop
+    : totalCountPerLoop;
+  try {
+    /**
+     * Get connections
+     */
+    const connections = await getConnectionToRds(process.env);
+
+    /**
+     * Get data from  db
+     */
+    const customerList = await getCustomerData(connections);
+    console.info("customerList", customerList);
+
+    currentCount = customerList.length;
+
+    for (let i = 0; i < customerList.length; i++) {
+      const customer_id = customerList[i].customer_id;
+      console.info("customer_id", customer_id);
+      try {
+        /**
+         * get customer from netsuit
+         */
+        const customerData = await getcustomer(customer_id);
+        console.info("customerData", customerData);
+        /**
+         * Update customer details into DB
+         */
+        await putCustomer(connections, customerData, customer_id);
+        console.info("count", i + 1);
+      } catch (error) {
+        let singleItem = "";
+        try {
+          if (error.hasOwnProperty("customError")) {
+            /**
+             * update error
+             */
+            singleItem = await getDataByCustomerId(connections, customer_id);
+            await updateFailedRecords(connections, customer_id);
+            await createARFailedRecords(
+              connections,
+              singleItem,
+              error,
+              arDbNamePrev
+            );
+          }
+        } catch (error) {
+          console.log("err", error);
+          await sendDevNotification(
+            source_system,
+            "AR",
+            "netsuite_customer_ar_m1 for loop customer_id" + customer_id,
+            singleItem,
+            error
+          );
+        }
+      }
+    }
+
+    if (currentCount > totalCountPerLoop) {
+      hasMoreData = "true";
+    } else {
+      hasMoreData = "false";
+    }
+  } catch (error) {
+    hasMoreData = "false";
+  }
+
+  if (hasMoreData == "false") {
+    return { hasMoreData };
+  } else {
+    return { hasMoreData };
+  }
+};
+
+async function getCustomerData(connections) {
+  try {
+    const query = `SELECT distinct customer_id FROM ${arDbName} 
+    where ((customer_internal_id is null and processed_date is null) or
+          (customer_internal_id is null and processed_date < '${today}'))
+          and source_system = '${source_system}'
+          limit ${totalCountPerLoop + 1}`;
+
+    console.info("query", query);
+    const [rows] = await connections.execute(query);
+    const result = rows;
+    if (!result || result.length == 0) {
+      throw "No data found.";
+    }
+    return result;
+  } catch (error) {
+    throw "getCustomerData: No data found.";
+  }
+}
+
+async function getDataByCustomerId(connections, cus_id) {
+  try {
+    const query = `SELECT * FROM ${arDbName} 
+                    where source_system = '${source_system}' and customer_id = '${cus_id}' 
+                    limit 1`;
+    console.log("query", query);
+    const [rows] = await connections.execute(query);
+    const result = rows;
+    if (!result || result.length == 0) {
+      throw "No data found.";
+    }
+    return result[0];
+  } catch (error) {
+    throw "getDataByCustomerId: No data found.";
+  }
+}
+
+async function getcustomer(entityId) {
+  try {
+    const options = {
+      consumer_key: userConfig.token.consumer_key,
+      consumer_secret_key: userConfig.token.consumer_secret,
+      token: userConfig.token.token_key,
+      token_secret: userConfig.token.token_secret,
+      realm: userConfig.account,
+      url: `${process.env.NS_CUSTOMER_URL}&deploy=1&custscript_mfc_entity_eid=${entityId}`,
+      method: "GET",
+    };
+    const authHeader = getAuthorizationHeader(options);
+
+    const configApi = {
+      method: options.method,
+      maxBodyLength: Infinity,
+      url: options.url,
+      headers: {
+        ...authHeader,
+      },
+    };
+
+    
+    const response =  await axios.request(configApi);
+
+    console.info("response", response.status);
+    console.info("response", response.data);
+
+    const recordList = response.data[0];
+    if (recordList && recordList.internalid) {
+      const record = recordList;
+      return record;
+    } else {
+      throw {
+        customError: true,
+        msg: `Customer not found. (customer_id: ${entityId})`,
+      };
+    }
+  } catch (err) {
+    console.error("error", err);
+    throw {
+      customError: true,
+      msg: `Customer not found. (customer_id: ${entityId})`, 
+    };
+  }
+}
+
+async function putCustomer(connections, customerData, customer_id) {
+  try {
+    const customer_internal_id = customerData.internal_id_value;
+
+    const formatData = {
+      customer_internal_id: customerData?.internal_id_value ?? "",
+      customer_id: customerData?.entityId_value ?? "",
+      currency_internal_id: customerData?.currency_internal_id_value,
+      curr_cd: customerData?.currency_internal_id_text,
+      currency_id: customerData?.currency_internal_id_value,
+      currency_refName: customerData?.currency_internal_id_text,
+      //externalId: customerData?.externalid ?? "",
+      custentity5: customerData?.custentity5_value ?? "",
+      custentity_2663_customer_refund:
+        customerData?.custentity_2663_customer_refund_value ?? "",
+      custentity_2663_direct_debit:
+        customerData?.custentity_2663_direct_debit_value ?? "",
+      custentity_ee_account_no: customerData?.custentity_ee_account_no_value ?? "",
+      custentity_riv_assigned_collector:
+        customerData?.custentity_riv_assigned_collector_value ?? "",
+      dateCreated: customerData?.dateCreated_value ?? "",
+      daysOverdue: customerData?.daysOverdue_value ?? "",
+      defaultAddress:
+        customerData?.defaultAddress_value.length > 0
+          ? customerData?.defaultAddress_value?.replace(/'/g, "`")
+          : "",
+      depositBalance: customerData?.depositBalance_value ?? "",
+      // autoName: customerData?.autoName ?? "",
+      balance: customerData?.balance_value ?? "",
+      companyName:
+        customerData?.companyName_value.length > 0
+          ? customerData?.companyName_value?.replace(/'/g, "`")
+          : "",
+      emailTransactions: customerData?.emailTransactions_value ?? "",
+      faxTransactions: customerData?.faxTransactions_value ?? "",
+      // isAutogeneratedRepresentingEntity:
+      //   customerData?.isAutogeneratedRepresentingEntity ?? "",
+      isInactive: customerData?.isInactive_value ?? "",
+      // isPerson: customerData?.isPerson ?? "",
+      lastModifiedDate: customerData?.lastModifiedDate_value ?? "",
+      overdueBalance: customerData?.overdueBalance_value ?? "",
+      printTransactions: customerData?.printTransactions_value ?? "",
+      unbilledOrders: customerData?.unbilledOrders_value ?? "",
+      shipComplete: customerData?.shipComplete_value ?? "",
+
+      // alcoholRecipientType_id: customerData?.alcoholRecipientType.id,
+      // alcoholRecipientType_refName: customerData?.alcoholRecipientType.refName,
+      creditHoldOverride_id: customerData?.creditHoldOverride_id_value,
+      creditHoldOverride_refName: customerData?.creditHoldOverride_id_text,
+      // customForm_id: customerData?.customForm.id,
+      // customForm_refName: customerData?.customForm.refName,
+      emailPreference_id: customerData?.emailPreference_id_value,
+      emailPreference_refName: customerData?.emailPreference_id_text,
+      entityStatus_id: customerData?.entityStatus_id_value,
+      entityStatus_refName: customerData?.entityStatus_id_text,
+      // receivablesAccount_id: customerData?.receivablesAccount.id,
+      receivablesAccount_refName: customerData?.receivablesAccount_id_value,
+      shippingCarrier_id: customerData?.shippingCarrier_id_value,
+      shippingCarrier_refName: customerData?.shippingCarrier_id_text,
+      subsidiary_id: customerData?.subsidiary_id_value,
+      subsidiary_refName: customerData?.subsidiary_id_text,
+      terms_id: customerData?.terms_id_value,
+      terms_refName: customerData?.terms_id_text,
+      created_at: moment().format("YYYY-MM-DD"),
+    };
+   
+
+    let tableStr = "";
+    let valueStr = "";
+    let updateStr = "";
+
+    let objKyes = Object.keys(formatData);
+    objKyes.map((e, i) => {
+      if (i > 0) {
+        valueStr += ",";
+        updateStr += e != "customer_id" ? "," : "";
+      }
+      if (e != "customer_id") {
+        updateStr += e + "='" + formatData[e] + "'";
+      }
+      valueStr += "'" + formatData[e] + "'";
+    });
+    tableStr = objKyes.join(",");
+
+    const upsertQuery = `INSERT INTO ${arDbNamePrev}netsuit_customer (${tableStr})
+                        VALUES (${valueStr}) ON DUPLICATE KEY
+                        UPDATE ${updateStr};`;
+    console.log("query", upsertQuery);
+    await connections.execute(upsertQuery);
+
+    const updateQuery = `UPDATE ${arDbName} SET 
+                    processed = null, 
+                    customer_internal_id = '${customer_internal_id}', 
+                    processed_date = '${today}' 
+                    WHERE customer_id = '${customer_id}' and source_system = '${source_system}' and customer_internal_id is null`;
+    console.log("updateQuery", updateQuery);
+    await connections.execute(updateQuery);
+  } catch (error) {
+    console.error(error);
+    throw "Customer Update Failed";
+  }
+}
+
+async function updateFailedRecords(connections, cus_id) {
+  try {
+    let query = `UPDATE ${arDbName}  
+                  SET processed = 'F',
+                  processed_date = '${today}' 
+                  WHERE customer_id = '${cus_id}' and source_system = '${source_system}' and customer_internal_id is null`;
+    console.log("query", query);
+    const result = await connections.execute(query);
+    return result;
+  } catch (error) { }
+}
+
+function getCustomDate() {
+  const date = new Date();
+  let ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
+  let mo = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(date);
+  let da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
+  return `${ye}-${mo}-${da}`;
+}
+
+async function checkOldProcessIsRunning() {
+  try {
+    //cw ar 
+    const customerArn = process.env.NETSUITE_AR_M1_CUSTOMER_STEP_ARN;
+    
+
+    const status = "RUNNING";
+    const stepfunctions = new AWS.StepFunctions();
+
+    const getExecutionList = async (stateMachineArn) => {
+      return new Promise((resolve, reject) => {
+        stepfunctions.listExecutions(
+          {
+            stateMachineArn,
+            statusFilter: status,
+            maxResults: 2,
+          },
+          (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data.executions);
+            }
+          }
+        );
+      });
+    };
+
+    const customerExcList = await getExecutionList(customerArn);
+    if (customerExcList.length === 2 && customerExcList[1].status === status) {
+      console.log("AR running");
+      return true;
+    }
+
+    return false;
+  } catch (error) {
+    return true;
+  }
+}
