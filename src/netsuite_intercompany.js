f2f644940f62ccc7d77fe04d15bf97b2df3c0b2b
tharun kancham <tharun.kncham@bizcloudexperts.com>
2024-01-02 16:58:14 -0600

    SNS Add for the Lambda functions

--- src/netsuite_intercompany.js
+++ src/netsuite_intercompany.js
@@ -1,4 +1,6 @@
 const AWS = require("aws-sdk");
+const {SNS_TOPIC_ARN } = process.env;
+const sns = new AWS.SNS({ region: process.env.REGION });
 const axios = require("axios");
 const crypto = require("crypto");
 const OAuth = require("oauth-1.0a");
@@ -68,6 +70,11 @@ module.exports.handler = async (event, context, callback) => {
     return { hasMoreData };
   } catch (error) {
     console.error("error:handler", error);
+    const params = {
+			Message: `Error in ${functionName}, Error: ${error.Message}`,
+			TopicArn: SNS_TOPIC_ARN,
+		};
+    await sns.publish(params).promise();
     await triggerReportLambda(
       process.env.NS_RESTLET_INVOICE_REPORT,
       "CW_INTERCOMPANY"
