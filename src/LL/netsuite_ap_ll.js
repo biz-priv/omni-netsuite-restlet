351d3113e668b24af8d049ad5c92a7b6ca90c7fc
madhavamvs25 <madhava.matta@bizcloudexperts.com>
2024-02-02 19:24:29 +0530

    updated ap-ll cancelled and completed queries

--- src/LL/netsuite_ap_ll.js
+++ src/LL/netsuite_ap_ll.js
@@ -585,7 +585,10 @@ async function cancellationProcess() {
   try {
     let cancelledData = [];
     try {
-      const query = `select distinct ae.invoice_nbr,ae.internal_id, ae.system_id, aes.status, ae.source_system from ${apDbName} ae join ${apDbNamePrev}interface_ap_epay_status aes
+      const query = `select distinct ae.invoice_nbr,ae.internal_id, ae.system_id, aes.status, ae.source_system from 
+      (select distinct invoice_nbr,internal_id,system_id,status,source_system,processed from ${apDbName} where processed='P'
+      union select distinct invoice_nbr,internal_id,system_id,status,source_system,processed from ${apDbNamePrev}interface_ap_epay_his) ae 
+      join ${apDbNamePrev}interface_ap_epay_status aes
       on ae.invoice_nbr=aes.invoice_nbr and ae.system_id=aes.system_id
       where ae.internal_id is not null and ae.processed ='P' and ((aes.processed is null) or (aes.processed = 'F' and aes.processed_date < '${today}')) and
       aes.status ='CANCELLED' limit ${totalCountPerLoop + 1}`;
@@ -737,10 +740,13 @@ async function billPaymentProcess() {
   try {
     let billPaymentData = [];
     try {
-      const query = `select ae.vendor_internal_id, ae.invoice_nbr,ae.internal_id,sum(ae.rate)-ae.discount, ae.system_id, aes.status, ae.source_system from ${apDbName} ae join ${apDbNamePrev}interface_ap_epay_status aes
-      on ae.invoice_nbr=aes.invoice_nbr
-      where ae.internal_id is not null and ae.processed ='P' and ((aes.processed is null) or (aes.processed = 'F' and aes.processed_date < '${today}')) and aes.status ='COMPLETED' 
-      group by ae.vendor_internal_id, ae.invoice_nbr,ae.internal_id, ae.system_id LIMIT ${totalCountPerLoop + 1}`;
+      const query = `select ae.vendor_internal_id, ae.invoice_nbr,ae.internal_id,sum(ae.rate)-COALESCE (ae.discount,0) as rate,ae.system_id, aes.status, ae.source_system
+      from (select distinct invoice_nbr,internal_id,system_id,status,source_system,processed,vendor_internal_id,rate,discount from ${apDbName} where processed='P'
+        union select distinct invoice_nbr,internal_id,system_id,status,source_system,processed,vendor_internal_id,rate,discount from ${apDbNamePrev}interface_ap_epay_his) ae
+      join ${apDbNamePrev}interface_ap_epay_status aes on ae.invoice_nbr=aes.invoice_nbr
+        where ae.internal_id is not null and ae.processed ='P'
+        and ((aes.processed is null) or (aes.processed = 'F' and aes.processed_date < '${today}')) and aes.status ='COMPLETED'
+        group by ae.vendor_internal_id, ae.invoice_nbr,ae.internal_id, ae.system_id LIMIT ${totalCountPerLoop + 1}`;
       billPaymentData = await fetchCancelAndBillPaymentData(query);
       currentCount = billPaymentData.length
     } catch (error) {
