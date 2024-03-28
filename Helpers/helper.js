6882780297cfaf5e40a1c9d9a203d5f77de5d7eb
jahir-biz <jahir.uddin@bizcloudexperts.com>
2024-03-28 18:34:21 +0530

    agw account creds

--- Helpers/helper.js
+++ Helpers/helper.js
@@ -37,6 +37,18 @@ function getConfig(source_system, env) {
       },
       wsdlPath: env.NETSUIT_AR_WDSLPATH,
     },
+    AG: {
+      account: env.NETSUIT_AR_ACCOUNT,
+      apiVersion: "2021_2",
+      accountSpecificUrl: true,
+      token: {
+        consumer_key: env.NETSUIT_AR_CONSUMER_KEY,
+        consumer_secret: env.NETSUIT_AR_CONSUMER_SECRET,
+        token_key: env.NETSUIT_AR_TOKEN_KEY,
+        token_secret: env.NETSUIT_AR_TOKEN_SECRET,
+      },
+      wsdlPath: env.NETSUIT_AR_WDSLPATH,
+    },
     M1: {
       account: env.NETSUIT_AR_ACCOUNT,
       apiVersion: "2021_2",
