63c18f1179ea9ef986e9d7e7341f1c45e8beacd0
jahir-biz <jahir.uddin@bizcloudexperts.com>
2024-03-28 16:47:39 +0530

    agw changes

--- src/invoice_report.js
+++ src/invoice_report.js
@@ -11,6 +11,10 @@ const mailList = {
     AR: process.env.NETSUIT_AR_ERROR_EMAIL_TO,
     AP: process.env.NETSUIT_AP_ERROR_EMAIL_TO,
   },
+  AG: {
+    AR: process.env.NETSUIT_AR_ERROR_EMAIL_TO,
+    AP: process.env.NETSUIT_AP_ERROR_EMAIL_TO,
+  },
   TR: {
     AR: process.env.NETSUIT_AR_TR_ERROR_EMAIL_TO,
     AP: process.env.NETSUIT_AP_TR_ERROR_EMAIL_TO,
@@ -153,6 +157,10 @@ async function getReportData(
         mainQuery = `select ${dbname}interface_ap.*, CONCAT('Vendor not found. (vendor_id: ', CAST(vendor_id AS CHAR), ') Subsidiary: ', subsidiary) AS error_msg
         from ${dbname}interface_ap where source_system = '${sourceSystem}' and processed ='F' and vendor_id in (${queryVenErr})
         GROUP BY invoice_nbr, vendor_id, invoice_type, gc_code, subsidiary, source_system;`;
+      } else if (sourceSystem == "AG") {
+        mainQuery = `select ${dbname}interface_ap.*, CONCAT('Vendor not found. (vendor_id: ', CAST(vendor_id AS CHAR), ') Subsidiary: ', subsidiary) AS error_msg
+        from ${dbname}interface_ap where source_system = '${sourceSystem}' and processed ='F' and vendor_id in (${queryVenErr})
+        GROUP BY invoice_nbr, vendor_id, invoice_type, gc_code, subsidiary, source_system;`;
       } else if (sourceSystem == "M1") {
         mainQuery = `select ${dbname}interface_ap.*, CONCAT('Vendor not found. (vendor_id: ', CAST(vendor_id AS CHAR), ') Subsidiary: ', subsidiary) AS error_msg
         from ${dbname}interface_ap where source_system = '${sourceSystem}' and processed ='F' and vendor_id in (${queryVenErr})
@@ -220,6 +228,10 @@ async function getReportData(
         mainQuery = `select ${dbname}interface_ar.*, CONCAT('Customer not found. (customer_id: ', CAST(customer_id AS CHAR), ') Subsidiary: ', subsidiary) AS error_msg
         from ${dbname}interface_ar where source_system = 'CW' and processed ='F' and customer_id in (${queryCuErr})
         GROUP BY invoice_nbr, invoice_type, gc_code, subsidiary`;
+      } else if (sourceSystem == "AG") {
+        mainQuery = `select ${dbname}interface_ar.*, CONCAT('Customer not found. (customer_id: ', CAST(customer_id AS CHAR), ') Subsidiary: ', subsidiary) AS error_msg
+        from ${dbname}interface_ar where source_system = 'CW' and processed ='F' and customer_id in (${queryCuErr})
+        GROUP BY invoice_nbr, invoice_type, gc_code, subsidiary`;
       } else if (sourceSystem == "M1") {
         mainQuery = `select ${dbname}interface_ar.*, CONCAT('Customer not found. (customer_id: ', CAST(customer_id AS CHAR), ') Subsidiary: ', subsidiary) AS error_msg
         from ${dbname}interface_ar where source_system = '${sourceSystem}' and processed ='F' and customer_id in (${queryCuErr})
@@ -271,6 +283,19 @@ async function getReportData(
     else {
       // INTERCOMPANY
       if (sourceSystem === "CW") {
+        if (intercompanyType === "AP") {
+          query=`select distinct ia.*,ial.error_msg,ial.id  from ${dbname}interface_ap ia 
+          join ${dbname}interface_intercompany_api_logs ial on ia.source_system=ial.source_system and 
+          ia.internal_id=ial.ap_internal_id and ia.file_nbr= ial.file_nbr
+          where ia.intercompany ='Y' and ial.source_system = 'CW' and ial.is_report_sent = 'N'`
+        } else {
+          query=`
+          select distinct ar.*, ial.error_msg, ial.id from ${dbname}interface_ar ar
+          join ${dbname}interface_intercompany_api_logs ial on ial.source_system = ar.source_system 
+          and ial.ar_internal_id  = ar.internal_id and ial.file_nbr = ar.file_nbr 
+          where ar.intercompany ='Y' and ial.source_system ='CW' and ial.is_report_sent ='N'`
+        }
+      }else if (sourceSystem === "AG") {
         if (intercompanyType === "AP") {
           query=`select distinct ia.*,ial.error_msg,ial.id  from ${dbname}interface_ap ia 
           join ${dbname}interface_intercompany_api_logs ial on ia.source_system=ial.source_system and 
